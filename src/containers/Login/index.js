c40a103c100daff0018ac8ff73f77cdf4466d244
Redhmam Pablo <redhmam@hotmail.com>
2020-02-22 19:38:47 +0000

    Login page implemented

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,13 +1,81 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import Banner from '../../components/Banner';
 
-const Login = () => {
+import {
+  Card,
+  Form,
+  Input,
+  Row,
+  Col,
+  Typography
+} from 'antd';
+
+import { 
+  Button, 
+  formItemLayout, 
+  tailFormItemLayout
+} from '../../components/UI';
+
+const { Title } = Typography;
+
+const Login = (props) => {
+
+  const { form } = props;
+  const { getFieldDecorator } = form;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   return (
     <Layout title="Login">
-      <Banner/>
+      <Row className="p-40">
+        <Col span={12} offset={6}>
+          <Card>
+            <Form {...formItemLayout} onSubmit={handleSubmit}>
+              <Form.Item {...tailFormItemLayout}>
+                <Title level={3}>Login</Title>
+              </Form.Item>
+              <Form.Item label="E-mail">
+                {getFieldDecorator('email', {
+                  rules: [
+                    {
+                      type: 'email',
+                      message: 'The input is not valid E-mail!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your E-mail!',
+                    },
+                  ],
+                })(<Input size="large"/>)}
+              </Form.Item>
+              <Form.Item label="Password" hasFeedback>
+                {getFieldDecorator('password', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your password!',
+                    }
+                  ],
+                })(<Input.Password  size="large"/>)}
+              </Form.Item>
+              <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit" size="large">
+                Login
+              </Button>
+            </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </Layout>
   );
 }
 
-export default Login;
+export default Form.create({ name: 'Login' })(Login);
